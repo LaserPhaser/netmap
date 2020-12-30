68548758efb7960a7f405cbfb46016570ffe94f4
Arseniy Antonov <arseny.antonov@gmail.com>
2020-12-30 16:04:15 +0300

    Added CLI and Readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # netmap
 Simple version of portscanner and netmap in GO
+
+
+Example of usage:
+
+````
+./netmap -port_start 0 -port_end 40000 -scan_protocol tcp -host localhost  
+Scanning started for localhost for ports range[0:40000], protocol tcp
+Found open port 1234
+Found open port 7742
+Found open port 5600
+Found open port 65530
+Scanning finished
+
--- cmd/netmap/netmap.go
+++ cmd/netmap/netmap.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"runtime"
@@ -10,28 +11,32 @@ import (
 )
 
 func main() {
-	numCpu := runtime.NumCPU()
+	portStart := flag.Int("port_start", 0, "scan from")
+	portEnd := flag.Int("port_end", 65535, "scan to")
+	scanProtocol := flag.String("scan_protocol", "tcp", "Protocol to use [TCP/UDP]")
+	scanHost := flag.String("host", "localhost", "Host to scan")
+	flag.Parse()
 
-	chunks := chunks(numCpu, 65535)
+	numCpu := runtime.NumCPU()
+	chunks := chunks(numCpu, *portStart, *portEnd)
 
 	var wg sync.WaitGroup
 	for _, ch := range chunks {
 		wg.Add(1)
-		go scanPort(&wg, ch[0], ch[1])
+		go scanPort(&wg, ch[0], ch[1], *scanProtocol, *scanHost)
 	}
-	fmt.Println("Scanning started")
-
+	fmt.Println("Scanning started for " + *scanHost + " for ports range[" + strconv.Itoa(*portStart) + ":" + strconv.Itoa(*portEnd) + "], protocol " + *scanProtocol)
 	wg.Wait()
 	fmt.Println("Scanning finished")
 
 }
 
-func scanPort(wg *sync.WaitGroup, from int, to int) {
+func scanPort(wg *sync.WaitGroup, from int, to int, scan_protocol string, scan_host string) {
 
 	defer wg.Done()
 	for port := from; port <= to; port++ {
 
-		conn, err := net.DialTimeout("tcp", "localhost:"+strconv.Itoa(port), time.Second)
+		conn, err := net.DialTimeout(scan_protocol, scan_host+":"+strconv.Itoa(port), time.Second)
 		if err == nil {
 			fmt.Println("Found open port", port)
 			conn.Close()
@@ -39,17 +44,17 @@ func scanPort(wg *sync.WaitGroup, from int, to int) {
 	}
 }
 
-func chunks(num_cpu int, max_value int) [][2]int {
+func chunks(num_cpu int, from int, to int) [][2]int {
 
 	var divided [][2]int
 	var chunk [2]int
-	chunkSize := (max_value + num_cpu - 1) / num_cpu
+	chunkSize := (to - from + num_cpu - 1) / num_cpu
 
-	for start := 0; start < max_value; start += chunkSize + 1 {
+	for start := from; start < to; start += chunkSize + 1 {
 		end := start + chunkSize
 
-		if end > max_value {
-			end = max_value
+		if end > to {
+			end = to
 		}
 		chunk[0] = start
 		chunk[1] = end
